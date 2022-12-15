8ef18f75231baa5ca93091624918dab77f0a54a7
korezz <koredeadebayo88@gmail.com>
2022-12-15 02:13:36 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,10 @@
-1 #include <stdio> 
-2 
-3/**
-4 * main - This prints Hello Precios
-5 *Return: This returns 0
-6 */
-7
-8 int main(void)
-9 {
-10       printf("Hello Precious\n"): /* Inline Comment*/
-11       printf("Hello to you too"):
-12       return (0):
-13 }	
+#include <stdio>  
+/**
+ * main - Entry
+ *Return: Alwaysb 0
+ */
+int main(void)
+{
+       puts("\"Programming is like building a multilingual puzzle");
+       return (0);
+}	
