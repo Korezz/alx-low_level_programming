4019c32d4148ec5fb360b5a1eb3b11ac827451c7
korezz <koredeadebayo88@gmail.com>
2022-12-29 14:29:57 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,14 @@
 #include <unistd.h>
 
-/**main - prints "and that piece of art is useful" - Dora korper, 2015-10-19",
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
  * followed by a newline, to stanard error.
  * Return: Always 1.
  */
+
 int main(void)
 
 {
-	write(2, "and that pieceof is useful\" - Dora kopar, 2015-10-19\n", 59);
+	write(2, "and that pieceof is useful\" - Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
